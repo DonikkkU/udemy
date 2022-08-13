18fafb090260b25cd9d5a325d829a30cba430ad6
Donik <umarovdoniyor096@gmail.com>
2022-08-14 03:12:35 +0500

    update

--- projects/urls.py
+++ projects/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('course/', CourseView.as_view()),
     path('course/<int:pk>', CourseDetailView.as_view()),
     path('category/', CategoryView.as_view()),
-    path('category/<int:pk>', CourseDetailView.as_view()),
+    path('category/<int:pk>', CategoryDetailView.as_view()),
     path('order/', OrderView.as_view()),
     path('order/<int:pk>', OrderDetailView.as_view()),
     path('homework/', HomeworkView.as_view()),
--- users/views.py
+++ users/views.py
@@ -2,11 +2,16 @@
 from .models import Profiles
 from .serializers import *
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 class ProfilesView(generics.ListCreateAPIView):
     queryset = Profiles.objects.all()
     serializer_class = ProfilesSerializer
 
+
 class ProfilesDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profiles.objects.all()
     serializer_class = ProfilesSerializer
+    filterset_fields = '__all__'
+    search_fields = ['-full_name']
+    ordering_fields = '__all__'
